145c2ed4100fbad2894ef3f7501e3cb7932f4cae
Ran Haveshush <ran.haveshush.dev@gmail.com>
2020-03-27 14:50:16 +0300

    Improve resource class.

--- app/src/main/java/com/example/baseapp/vo/Resource.kt
+++ app/src/main/java/com/example/baseapp/vo/Resource.kt
@@ -4,10 +4,10 @@ package com.example.baseapp.vo
  * The resource is comprised of the [resource state][State],
  * and the resource data of type [T].
  */
-data class Resource<T> private constructor(
-    val state: State,
-    val data: T? = null
-) {
+sealed class Resource<out T> {
+    abstract val state: State
+    abstract val data: T?
+
     /**
      * Resource current status.
      */
@@ -21,45 +21,70 @@ data class Resource<T> private constructor(
      * The resource state comprised of the resource current [Status]
      * and in case of an error the error message and the error [cause][Throwable].
      */
-    data class State private constructor(
-        val status: Status,
-        val message: String? = null,
-        val cause: Throwable? = null
-    ) {
-        companion object {
-            val LOADING =
-                State(Status.LOADING)
-            val SUCCESS =
-                State(Status.SUCCESS)
+    sealed class State {
+        abstract val status: Status
+        abstract val message: String?
 
-            fun error(message: String, cause: Throwable? = null) =
-                State(
-                    Status.FAILURE,
-                    message,
-                    cause
-                )
+        data class LoadingState(
+            override val status: Status,
+            override val message: String? = null
+        ) : State()
+        data class SuccessState(
+            override val status: Status,
+            override val message: String? = null
+        ) : State()
+        data class ErrorState(
+            override val status: Status,
+            override val message: String,
+            val cause: Throwable? = null
+        ) : State()
+
+        companion object {
+            val LOADING = LoadingState(Status.LOADING)
+            val SUCCESS = SuccessState(Status.SUCCESS)
+
+            fun error(message: String, cause: Throwable? = null) = ErrorState(
+                Status.FAILURE,
+                message,
+                cause
+            )
         }
     }
+
+    data class LoadingResource<T>(
+        override val state: State.LoadingState,
+        override val data: T? = null
+    ) : Resource<T>()
+    data class EmptyResource<T>(
+        override val state: State.SuccessState,
+        override val data: T? = null
+    ) : Resource<T>()
+    data class SuccessResource<T>(
+        override val state: State.SuccessState,
+        override val data: T
+    ) : Resource<T>()
+    data class ErrorResource<T>(
+        override val state: State.ErrorState,
+        override val data: T? = null
+    ) : Resource<T>()
 
     companion object {
         /**
          * Convenient method to build a loading resource.
          */
-        fun <T> loading(data: T? = null) =
-            Resource(
-                State.LOADING,
-                data
-            )
+        fun <T> loading() = LoadingResource<T>(State.LOADING)
+
+        /**
+         * Convenient method to build an empty success resource.
+         */
+        fun <T> empty() = EmptyResource<T>(State.SUCCESS)
 
         /**
          * Convenient method to build a success resource with a given data.
          *
          * @param data The data held by the resource.
          */
-        fun <T> success(data: T) = Resource(
-            State.SUCCESS,
-            data
-        )
+        fun <T> success(data: T) = SuccessResource(State.SUCCESS, data)
 
         /**
          * Convenient method to build a failure resource with a given error message and
@@ -68,12 +93,11 @@ data class Resource<T> private constructor(
          * @param message The failure error message.
          * @param cause The failure error throwable cause, if exists.
          */
-        fun <T> error(message: String, cause: Throwable? = null) =
-            Resource<T>(
-                State.error(
-                    message,
-                    cause
-                )
+        fun <T> error(message: String, cause: Throwable? = null) = ErrorResource<T>(
+            State.error(
+                message,
+                cause
             )
+        )
     }
 }
